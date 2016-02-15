b705e3312f922d4bc85435cf9c99f1dbe10de712
Busato <busato@in2p3.fr>
2016-02-15 22:06:21 +0100

    update

--- dpga/rw/writer.go
+++ dpga/rw/writer.go
@@ -5,6 +5,9 @@ import (
 	"encoding/binary"
 	"fmt"
 	"io"
+	"log"
+
+	"gitlab.in2p3.fr/avirm/analysis-go/dpga/event"
 )
 
 // Writer wraps an io.Writer and writes an ASM stream.
@@ -128,24 +131,46 @@ func (w *Writer) writeBlockData(blk *Block) {
 	}
 }
 
-/*
-func (w *Writer) WriteNextEvent(event *event.Event) {
+func (w *Writer) WriteEvent(event *event.Event) {
 	// Build frames for this event
 	// One event is made of 120 frames
-	var frames []Frame
+	frames := make([]Frame, 120)
+	iframe := 0
 	for i := range event.Clusters {
-		cluster := &event.Clusters[i]
+		if iframe >= 120 {
+			log.Fatalf("rw: iframe out of range")
+		}
 
-		frame1 := &Frame{ID: 0}
-		frame2 := &Frame{ID: 0}
+		frame1 := &frames[iframe]
+		frame2 := &frames[iframe+1]
 
 		block1 := &frame1.Block
 		block2 := &frame2.Block
 
-		block1.Evt = event.ID
-		block2.Evt = event.ID
+		block1.Evt = uint32(event.ID)
+		block2.Evt = uint32(event.ID)
+
+		cluster := &event.Clusters[i]
+
+		if cluster.NoSamples() != numSamples {
+			log.Fatalf("rw: cluster.NoSamples() != numSamples")
+		}
+
+		pulses := &cluster.Pulses
+
+		for j := uint16(0); j < numSamples; j++ {
+			// Make block1 data from pulse[0] and pulse[1]
+			amp0, amp1 := uint16(pulses[0].Samples[j].Amplitude), uint16(pulses[1].Samples[j].Amplitude)
+			var word uint32
+
+			block1.Data = append(block1.Data, word)
+			// Make block2 data from pulse[2] and pulse[3]
+			block2.Data = append(block2.Data, uint32(0))
+		}
 
 		frames = append(frames, *frame1, *frame2)
+
+		iframe += 2
 	}
 
 	// write frames
@@ -153,4 +178,3 @@ func (w *Writer) WriteNextEvent(event *event.Event) {
 		log.Fatalf("rw: number of frames not correct, expect 120, get %v", len(frames))
 	}
 }
-*/
