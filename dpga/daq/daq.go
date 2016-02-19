58ebaab5777356d978edbc59d40025901a8e0c65
Busato <busato@in2p3.fr>
2016-02-19 10:20:57 +0100

    update

--- dpga/daq/daq.go
+++ dpga/daq/daq.go
@@ -3,10 +3,12 @@ package main
 import (
 	"bufio"
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net"
 	"os"
+	"sync"
 
 	"gitlab.in2p3.fr/avirm/analysis-go/dpga/rw"
 )
@@ -15,13 +17,15 @@ func main() {
 	log.SetFlags(log.Llongfile | log.LstdFlags)
 
 	var (
-		noEvents = flag.Uint("n", 1000, "Number of events")
+		noEvents    = flag.Uint("n", 1000, "Number of events")
 		outfileName = flag.String("o", "out.bin", "Name of the output file")
+		port        = flag.String("p", "1024", "Port number")
 	)
 
 	flag.Parse()
 
-	laddr, err := net.ResolveTCPAddr("tcp", "192.168.100.11:1024")
+	// Reader
+	laddr, err := net.ResolveTCPAddr("tcp", "192.168.100.11:"+*port)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -29,8 +33,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// Reader
+	
 	r, err := rw.NewReader(bufio.NewReader(tcp))
 	if err != nil {
 		log.Fatalf("could not open stream: %v\n", err)
@@ -58,9 +61,23 @@ func main() {
 		log.Fatalf("error writing header: %v\n", err)
 	}
 
+	// Start goroutines
+	const N = 2
+	var wg sync.WaitGroup
+	wg.Add(N)
+	go stream(r, w, noEvents, &wg)
+	go command(&wg)
+	wg.Wait()
+}
+
+func stream(r *rw.Reader, w *rw.Writer, noEvents *uint, wg *sync.WaitGroup) {
+	defer wg.Done()
 	nFrames := uint(0)
-	for nFrames / 120 < *noEvents {
+	for nFrames/120 < *noEvents {
+		//start := time.Now()
 		frame, err := r.Frame()
+		//duration := time.Since(start)
+// 		time.Sleep(1 * time.Millisecond)
 		if err != nil {
 			if err != io.EOF {
 				log.Fatalf("error loading frame: %v\n", err)
@@ -77,6 +94,38 @@ func main() {
 		nFrames++
 	}
 }
+
+func command(wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		in := bufio.NewReader(os.Stdin) //Scanner
+		word, _ := in.ReadString('\n')
+		word = strings.Replace(word, "\n", "", -1)
+		switch word {
+		default:
+			fmt.Println("do nothing !", word)
+		case "q":
+			fmt.Println("quit !")
+			return
+		}
+	}
+}
+
+/*
+func command(e interface{}) {
+	for {
+		switch e := e.(type) {
+		case key.Event:
+			switch e.Code {
+			case key.CodeEscape, key.CodeQ:
+				if e.Direction == key.DirPress {
+					return
+				}
+			}
+		}
+	}
+}
+*/
 
 /*
 // OLD STUFF, TO BE REMOVED
