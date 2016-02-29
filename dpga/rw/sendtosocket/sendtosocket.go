7f0bf58271c9d3aa7dc0c31d16af9528e3657519
Busato <busato@in2p3.fr>
2016-02-29 11:29:32 +0100

    update

--- dpga/rw/sendtosocket/sendtosocket.go
+++ dpga/rw/sendtosocket/sendtosocket.go
@@ -1,5 +1,80 @@
 package sendtosocket
 
+import (
+	"bufio"
+	"flag"
+	"io"
+	"log"
+	"net"
+	"os"
+
+	"gitlab.in2p3.fr/avirm/analysis-go/dpga/rw"
+)
+
 func main() {
-	
+	log.SetFlags(log.Llongfile | log.LstdFlags)
+
+	var (
+		fileName = flag.String("i", "", "Input file name")
+		ip       = flag.String("ip", "192.168.100.11", "IP address")
+		port     = flag.String("p", "1024", "Port number")
+	)
+
+	flag.Parse()
+
+	// Reader
+	filew, err := os.Open(*fileName)
+	if err != nil {
+		log.Fatalf("could not create data file: %v\n", err)
+	}
+	defer filew.Close()
+	r, err := rw.NewReader(bufio.NewReader(filew))
+	if err != nil {
+		log.Fatalf("could not open stream: %v\n", err)
+	}
+
+	// Writer
+	laddr, err := net.ResolveTCPAddr("tcp", *ip+":"+*port)
+	if err != nil {
+		log.Fatal(err)
+	}
+	tcp, err := net.DialTCP("tcp", nil, laddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w := rw.NewWriter(bufio.NewWriter(tcp))
+	if err != nil {
+		log.Fatalf("could not open file: %v\n", err)
+	}
+	defer w.Close()
+
+	// Start writing stream to TCP
+	hdr := r.Header()
+	hdr.Print()
+
+	err = w.Header(hdr)
+	if err != nil {
+		log.Fatalf("error writing header: %v\n", err)
+	}
+
+	for {
+		frame, err := r.Frame()
+		//duration := time.Since(start)
+		//time.Sleep(1 * time.Millisecond)
+		if err != nil {
+			if err != io.EOF {
+				log.Fatalf("error loading frame: %v\n", err)
+			}
+			if frame.ID != rw.LastFrame() {
+				log.Fatalf("invalid last frame id. got=%d. want=%d", frame.ID, rw.LastFrame())
+			}
+			break
+		}
+		err = w.Frame(*frame)
+		if err != nil {
+			log.Fatalf("error writing frame: %v\n", err)
+		}
+	}
+
 }
