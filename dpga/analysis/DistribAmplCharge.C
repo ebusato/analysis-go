efe839dbca61488febdb57c6585ca18301194388
Busato <busato@in2p3.fr>
2016-10-13 15:31:21 +0200

    update

--- dpga/analysis/DistribAmplCharge.C
+++ dpga/analysis/DistribAmplCharge.C
@@ -4,7 +4,31 @@
 #include <TTreeReaderValue.h>
 #include <TTreeReaderArray.h>
 
+TF1* Fit(TH1F* h) {
+	Double_t low = 1400;
+	Double_t high = 3000;
+	TF1* f = 0;
+	for(int i = 0; i < 3; i++) {
+		h->Fit("gaus", "Q", "", low, high);
+		f = h->GetFunction("gaus");
+		Double_t mean = f->GetParameter(1);
+		Double_t sigma = f->GetParameter(2);
+		if(mean == 0) {
+			cout << "ERROR: fitted mean is zero" << endl;
+			return 0;
+		}
+		if(sigma > 0.20*2000) {
+			sigma = 0.15*2000;
+		}
+		low = mean - 2 * sigma;
+		high = mean + 2 * sigma;
+	}
+	return f;
+}
+
 void DistribAmplCharge(TString fileName) {
+	gStyle->SetOptStat(0);
+	gStyle->SetOptTitle(0);
 	TFile* f = new TFile(fileName, "read");
 	
         TTreeReader reader("tree", f);
@@ -39,8 +63,16 @@ void DistribAmplCharge(TString fileName) {
 	
 	TCanvas* cLeft = new TCanvas("cLeft", "cLeft", 1500, 800);
 	TCanvas* cRight = new TCanvas("cRight", "cRight", 1500, 800);
+	cLeft->SetFillColor(7);
+	cRight->SetFillColor(kYellow);
 	cLeft->Divide(5, 6);
 	cRight->Divide(5, 6);
+	
+	std::time_t t = std::time(0);  // t is an integer type
+	ofstream of("energy.csv");
+	of << "# LAPD energy calibration constants (creation date: " << t << ", input file: " << fileName.Data() <<  ")" << endl;
+	of << "# Calibration constant defined as the number of ADC counts corresponding to 511 keV" << endl;
+        of << "# iChannelAbs240 calibConstant calibConstantError " << endl;
 	
 	// Draw right hemisphere
 	for (int iQ = 0; iQ < 60; iQ++) {
@@ -56,7 +88,12 @@ void DistribAmplCharge(TString fileName) {
 			int ipad = irow*5 + icol;
 			cLeft->cd(ipad);
 		}
-		TLegend* leg = new TLegend(0.48,0.48,0.90,0.74);
+		gPad->SetFillColor(kWhite);
+// 		gPad->SetBottomMargin(0);
+// 		gPad->SetTopMargin(0);
+// 		gPad->SetLeftMargin(0);
+// 		gPad->SetRightMargin(0);
+		TLegend* leg = new TLegend(0.6,0.6,1,1);
 		for (int iC = 0; iC < 4; iC++) {
 			int iChanAbs240 = 4*iQ + iC;
 			int color;
@@ -73,14 +110,29 @@ void DistribAmplCharge(TString fileName) {
 			histos[iChanAbs240]->SetLineWidth(1);
 			histos[iChanAbs240]->SetFillStyle(3001);
 			histos[iChanAbs240]->SetFillColor(color);
+			histos[iChanAbs240]->GetXaxis()->SetLabelSize(0.055);
+			histos[iChanAbs240]->GetYaxis()->SetLabelSize(0.055);
 			if(iC == 0) {
 				histos[iChanAbs240]->Draw();
 			} else {
 				histos[iChanAbs240]->Draw("same");
+			}
+			TF1* fitfunc = Fit(histos[iChanAbs240]);
+			if(fitfunc) {
+				Double_t mean = fitfunc->GetParameter(1);
+				Double_t sigma = fitfunc->GetParameter(2);
+				Double_t meanErr = fitfunc->GetParError(1);
+				cout << iChanAbs240 << "   " << mean << "  " << meanErr << "  " << sigma << endl;
+				if(sigma < 500 && meanErr < 0.20*mean) {
+					of << iChanAbs240 << " " << mean << " " << meanErr << endl;
+				} else {
+					of << iChanAbs240 << " 0 0" << endl;
+				}
 			}
 			leg->AddEntry(histos[iChanAbs240], Form("iChanAbs240=%i", iChanAbs240), "f");
 		}
 		leg->SetLineWidth(0);
 		leg->Draw();
 	}
+	of.close();
 }
