bb82d371c0600a703c7c897cc1c8ab9629988c40
Busato <busato@in2p3.fr>
2017-02-02 09:14:45 +0100

    update

--- dpga/calib/selectCalib/selectCalib.go
+++ dpga/calib/selectCalib/selectCalib.go
@@ -3,16 +3,31 @@
 package selectCalib
 
 import (
+	"log"
 	"os"
+	"strings"
 
 	"gitlab.in2p3.fr/avirm/analysis-go/dpga/dpgadetector"
 )
 
 func Which(s string) {
-	switch s {
-	case "A1":
-		dpgadetector.Det.ReadPedestalsFile(os.Getenv("HOME") + "/godaq/calib/periodA/v1/pedestals.csv")
-		dpgadetector.Det.ReadTimeDepOffsetsFile(os.Getenv("HOME") + "/godaq/calib/periodA/v1/timeDepOffsets.csv")
-		dpgadetector.Det.ReadEnergyCalibFile(os.Getenv("HOME") + "/godaq/calib/periodA/v1/energy.csv")
+	chars := strings.Split(s, "")
+	pedFile := os.Getenv("HOME") + "/godaq/calib/period" + chars[0] + "/v" + chars[1] + "/pedestals.csv"
+	tdoFile := os.Getenv("HOME") + "/godaq/calib/period" + chars[0] + "/v" + chars[1] + "/timeDepOffsets.csv"
+	enFile := os.Getenv("HOME") + "/godaq/calib/period" + chars[0] + "/v" + chars[1] + "/energy.csv"
+	if _, err := os.Stat(pedFile); err == nil {
+		dpgadetector.Det.ReadPedestalsFile(pedFile)
+	} else {
+		log.Printf("pedestal file %s does not exist\n", pedFile)
+	}
+	if _, err := os.Stat(tdoFile); err == nil {
+		dpgadetector.Det.ReadTimeDepOffsetsFile(tdoFile)
+	} else {
+		log.Printf("tdo file %s does not exist\n", tdoFile)
+	}
+	if _, err := os.Stat(enFile); err == nil {
+		dpgadetector.Det.ReadEnergyCalibFile(enFile)
+	} else {
+		log.Printf("en file %s does not exist\n", enFile)
 	}
 }
