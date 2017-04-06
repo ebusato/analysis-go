c94ed1fe83a4cc8894d46c605c3fe3bf90750a98
Busato <busato@in2p3.fr>
2017-04-06 11:11:03 +0200

    add go.sh

---
+++ dpga/calib/computePedestals/go.sh
@@ -0,0 +1,14 @@
+
+computePedestals -i v2.6/run2.bin -o output/pedestals_run2.csv
+computePedestals -i v2.7/run22.bin -o output/pedestals_run22.csv
+computePedestals -i v2.8/run38.bin -o output/pedestals_run38.csv
+computePedestals -i v2.8/run39.bin -o output/pedestals_run39.csv
+computePedestals -i v2.8/run41.bin -o output/pedestals_run41.csv
+computePedestals -i v2.9/run46.bin -o output/pedestals_run46.csv
+computePedestals -i v2.10/run62.bin -o output/pedestals_run62.csv
+computePedestals -i v2.10/run68.bin -o output/pedestals_run68.csv
+computePedestals -i v2.10/run72.bin -o output/pedestals_run72.csv
+computePedestals -i v2.10/run75.bin -o output/pedestals_run75.csv
+computePedestals -i v2.10/run81.bin -o output/pedestals_run81.csv
+computePedestals -i v2.10/run87.bin -o output/pedestals_run87.csv
+computePedestals -i v2.10/run97.bin -o output/pedestals_run97.csv
