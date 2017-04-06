516fe46442f9f63c32befa9c942915905ac09a44
Busato <busato@in2p3.fr>
2017-04-06 12:01:11 +0200

    update

--- dpga/calib/computePedestals/go.sh
+++ dpga/calib/computePedestals/go.sh
@@ -1,14 +1,39 @@
 
-computePedestals -i v2.6/run2.bin -o output/pedestals_run2.csv
+# Tue Jan 17 15:25:39 CET 2017
+computePedestals -i v2.6/run2.bin -o csvs/pedestals_run2.csv
+
+# Thu Feb  2 16:45:59 CET 2017
 computePedestals -i v2.7/run22.bin -o output/pedestals_run22.csv
+
+# Tue Feb  7 13:45:15 CET 2017
 computePedestals -i v2.8/run38.bin -o output/pedestals_run38.csv
+
+# Wed Feb  8 09:22:31 CET 2017
 computePedestals -i v2.8/run39.bin -o output/pedestals_run39.csv
+
+# Thu Feb  9 09:36:30 CET 2017
 computePedestals -i v2.8/run41.bin -o output/pedestals_run41.csv
+
+# Fri Feb 10 09:54:54 CET 2017
 computePedestals -i v2.9/run46.bin -o output/pedestals_run46.csv
+
+# Wed Feb 15 15:19:03 CET 2017
 computePedestals -i v2.10/run62.bin -o output/pedestals_run62.csv
+
+# Fri Feb 17 09:16:35 CET 2017
 computePedestals -i v2.10/run68.bin -o output/pedestals_run68.csv
+
+# Sat Feb 18 10:26:48 CET 2017
 computePedestals -i v2.10/run72.bin -o output/pedestals_run72.csv
+
+# Sun Feb 19 10:22:48 CET 2017
 computePedestals -i v2.10/run75.bin -o output/pedestals_run75.csv
+
+# Tue Mar 14 09:26:13 CET 2017
 computePedestals -i v2.10/run81.bin -o output/pedestals_run81.csv
+
+# Wed Mar 15 11:38:41 CET 2017
 computePedestals -i v2.10/run87.bin -o output/pedestals_run87.csv
+
+# Thu Mar 16 12:49:29 CET 2017
 computePedestals -i v2.10/run97.bin -o output/pedestals_run97.csv
